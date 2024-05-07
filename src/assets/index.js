0ddbd1d80a50950233118a59105f5d62907f279b
MikiZenebe <mickyaszenebe@gmail.com>
2024-05-06 20:50:16 -0500

    initial

--- src/assets/index.js
+++
@@ -1,5 +0,0 @@
-import NoProfile from "./userprofile.png";
-import BgImage from "./img.jpeg";
-import Logo from "./logo.png";
-
-export { NoProfile, BgImage, Logo };
--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from "./Home";
-import Login from "./Login";
-import Profile from "./Profile";
-import Register from "./Register";
-import ResetPassword from "./ResetPassword";
-
-export { Home, Login, Profile, Register, ResetPassword };
