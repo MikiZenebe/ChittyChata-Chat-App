f333a0b2046c6488229396402daf5c8006e0ba4f
MikiZenebe <mickyaszenebe@gmail.com>
2024-05-06 21:05:26 -0500

    server-setup

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+//Connect to mongodb
+const dbConnect = async () => {
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("Database is running");
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+  dbConnect();
+});
