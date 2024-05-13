a4e12c1262d334f41950c29fd5f80bd75c086eec
MikiZenebe <mickyaszenebe@gmail.com>
2024-05-12 21:10:05 -0500

    avatar

--- client/src/pages/SetAvatar.jsx
+++ client/src/pages/SetAvatar.jsx
@@ -1,91 +1,61 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { avatar } from "../utils/multiAvatar";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import { Buffer } from "buffer";
-import { toast } from "react-toastify";
-import { useEffect, useState } from "react";
-import { setAvatarRoute } from "../utils/ApiRoutes";
+import { useState, useRef } from "react";
+import usePreviewImg from "../utils/usePreviewImg";
 import loader from "../assets/loader.gif";
+import { BsFillImageFill } from "react-icons/bs";
 
 export default function SetAvatar() {
-  const api = avatar;
-  const navigate = useNavigate();
-  const [avatars, setAvatars] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [selectedAvatar, setSelectedAvatar] = useState(undefined);
-
-  useEffect(() => {
-    if (!localStorage.getItem("user")) navigate("/login");
-  }, [navigate]);
-
-  const setProfilePicture = async () => {
-    if (selectedAvatar === undefined) {
-      toast.error("Please select an avatar");
-    } else {
-      const user = await JSON.parse(localStorage.getItem("user"));
-
-      const { data } = await axios.post(`${setAvatarRoute}/${user._id}`, {
-        image: avatars[selectedAvatar],
-      });
-
-      if (data.isSet) {
-        user.isAvatarImageSet = true;
-        user.avatarImage = data.image;
-        localStorage.setItem("user", JSON.stringify(user));
-        navigate("/");
-      } else {
-        toast.error("Error setting avatar. Please try again.");
-      }
-    }
-  };
-
-  useEffect(() => {
-    const data = [];
-    // for (let i = 0; i < 4; i++) {
-    //   const image = axios.get(`${api}/${Math.round(Math.random() * 1000)}`);
-    //   const buffer = new Buffer(image.data);
-    //   data.push(buffer.toString("base64"));
-    // }
-    setAvatars(data);
-    setIsLoading(false);
-  }, []);
+  const imageRef = useRef(null);
+  const { handleImageChange, imgUrl } = usePreviewImg();
 
   return (
-    <>
-      {isLoading ? (
-        <div className="flex justify-center items-center flex-col gap-[3rem] h-[100vh] w-[100vw]">
+    <div className="bg">
+      {!isLoading ? (
+        <div className="flex items-center justify-center flex-col gap-[3rem] h-[100vh] w-[100vw]">
+          {" "}
           <img src={loader} alt="loader" className="loader" />
         </div>
       ) : (
-        <div>
-          <div className="title-container">
-            <h1>Pick an Avatar as your profile picture</h1>
-          </div>
-          <div className="avatars">
-            {avatars.map((avatar, index) => {
-              return (
-                <div
-                  key={index}
-                  className={`avatar ${
-                    selectedAvatar === index ? "selected" : ""
-                  }`}
-                >
+        <div className="flex items-center justify-center flex-col gap-[3rem] h-[100vh] w-[100vw] ">
+          <form className="flex gap-2 flex-col bg-white rounded-lg py-[2rem] px-[3rem]">
+            <h1 className="font-bold text-2xl">
+              Pick an Avatar as your profile picture
+            </h1>
+            <div className="mx-auto  flex items-center justify-center transition-all ease-in-out">
+              <input
+                type="file"
+                hidden
+                ref={imageRef}
+                onChange={handleImageChange}
+              />
+
+              {imgUrl ? (
+                <div className="w-full mt-5 ">
                   <img
-                    src={`data:image/png;base64,${avatar}`}
-                    alt="avatar"
-                    key={avatar}
-                    onClick={() => setSelectedAvatar(index)}
+                    className="w-[100px] h-[100px] object-cover rounded-full"
+                    src={imgUrl}
+                    alt="Selected img"
                   />
                 </div>
-              );
-            })}
-          </div>
-          <button onClick={setProfilePicture} className="submit-btn">
-            Set as Profile Picture
-          </button>
+              ) : (
+                <div className="rounded-full">
+                  <BsFillImageFill
+                    className="ml-[5px] cursor-pointer"
+                    size={50}
+                    onClick={() => imageRef.current.click()}
+                  />
+                </div>
+              )}
+            </div>
+            <button
+              className="mx-auto w-full transition-all duration-[300ms] ease-out text-white bg-[#0940b0]  h-[40px] rounded-lg hover:bg-[#4279e9] hover:text-white disabled:cursor-not-allowed disabled:btn-disabled  uppercase mt-3"
+              type="submit"
+            >
+              Set as Profile Picture
+            </button>
+          </form>
         </div>
       )}
-    </>
+    </div>
   );
 }
