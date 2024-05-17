8b288704219350301981a14ab20227804f2c8dfa
MikiZenebe <mickyaszenebe@gmail.com>
2024-05-16 21:26:13 -0500

    init

--- client/src/components/Contact.jsx
+++
@@ -1,4 +0,0 @@
-export default function Contact({ contacts, currentUser }) {
-  console.log(contacts);
-  return <div>Contact</div>;
-}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/ReactToastify.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ToastContainer
-        position="top-center"
-        autoClose={1500}
-        pauseOnHover={true}
-        draggable={true}
-        limit={1}
-      />
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
