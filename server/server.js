c726609895cf75e97d4e39130e7b69e3617498a6
MikiZenebe <mikizenebe10gmail.com>
2024-08-15 23:58:13 +0300

    server - user

---
+++ server/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./src/db/connectDB.js";
+import { createServer } from "node:http";
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+const app = express();
+// const httpServer = new createServer(app);
+
+//middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const server = async () => {
+  try {
+    await connectDB();
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log("Failed to strt server.....", error.message);
+    process.exit(1);
+  }
+};
+
+server();
