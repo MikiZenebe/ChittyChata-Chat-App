5979a5a7bba5613ec090936016dc9d059edfe2b1
MikiZenebe <mikizenebe10gmail.com>
2024-08-22 00:05:52 +0300

    profile-ui

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,6 +1,24 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const colors = [
+  // "bg-[#712c4a57] text-[#ff006e] border-[1px] border-[#ff006faa]",
+  // "bg-[#ffd60a2a] text-[#ffd60a] border-[1px] border-[#ffd60abb]",
+  // "bg-[#06d6a02a] text-[#06d6a0] border-[1px] border-[#06d6a0bb]",
+  // "bg-[#4cc9f02a] text-[#4cc9f0] border-[1px] border-[#4cc9f0bb]",
+  "bg-gradient-to-r from-[#712c4a57] to-[#ff006e] text-red-100 border-[1px] border-[#712c4a57]",
+  "bg-gradient-to-r from-[#ffd60a2a] to-[#ffd60a] text-yellow-50 border-[1px] border-[#ffd60a2a]",
+  "bg-gradient-to-r from-[#06d6a02a] to-[#06d6a0] text-green-100 border-[1px] border-[#06d6a02a]",
+  "bg-gradient-to-r from-[#4cc9f02a] to-[#4cc9f0] text-blue-50 border-[1px] border-[#4cc9f02a]",
+];
+
+export const getColor = (color) => {
+  if (color >= 0 && color < colors.length) {
+    return colors[color];
+  }
+  return colors[0];
+};
--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,7 +1,124 @@
 import { useAppStore } from "@/store";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { colors, getColor } from "@/lib/utils";
+import { FaPlus, FaTrash } from "react-icons/fa";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function Profile() {
+  const navigate = useNavigate();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [image, setImage] = useState(null);
+  const [hoverd, setHoverd] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(0);
   const { userInfo } = useAppStore();
-  console.log(userInfo);
-  return <div>{userInfo.email}</div>;
+
+  const saveChanges = async () => {};
+
+  return (
+    <div className="h-[100vh] flex items-center justify-center flex-col">
+      <div className="p-4 flex flex-col gap-10 w-[80vw] md:w-max  border-2 border-slate-200 rounded-3xl">
+        <div className="flex items-center">
+          <IoArrowBack className="text-4xl text-slate-400 cursor-pointer " />
+          <h3 className="text-center mx-auto text-2xl font-bold bg-gradient-to-r from-[#62E1FB] to-[#27BBF6] inline-block text-transparent bg-clip-text">
+            Setup Profile
+          </h3>
+        </div>
+
+        <div className="grid grid-cols-2 items-center">
+          <div
+            className="h-36 w-36 sm:w-44 sm:h-44 md:w-56 md:h-56 relative flex items-center justify-center "
+            onMouseEnter={() => setHoverd(true)}
+            onMouseLeave={() => setHoverd(false)}
+          >
+            <Avatar className="w-28 h-28 md:w-36 md:h-36 object-cover rounded-full overflow-hidden transition-all duration-300 ease-out">
+              {image ? (
+                <AvatarImage
+                  src={image}
+                  alt="profile"
+                  className="object-cover w-full h-full bg-black"
+                />
+              ) : (
+                <div
+                  className={`uppercase h-28 w-28 md:w-36 md:h-36 text-5xl border-[1px] flex items-center justify-center rounded-full ${getColor(
+                    selectedColor
+                  )}`}
+                >
+                  {firstName
+                    ? firstName.split("").shift()
+                    : userInfo.email.split("").shift()}
+                </div>
+              )}
+            </Avatar>
+
+            {hoverd && (
+              <div className="absolute inset-4 sm:inset-8  md:inset-10 flex items-center justify-center bg-black/50 ring-fuchsia-50 cursor-pointer rounded-full transition-all duration-300 ease-out">
+                {image ? (
+                  <FaTrash className="text-white text-3xl cursor-pointer" />
+                ) : (
+                  <FaPlus className="text-white text-3xl cursor-pointer" />
+                )}
+              </div>
+            )}
+          </div>
+
+          <div className="flex min-w-32 md:min-w-64 flex-col gap-5 items-center justify-center">
+            <div className="w-full">
+              <Input
+                placeholder="Email"
+                type="email"
+                disabled
+                value={userInfo.email}
+                className="rounded-lg p-6 border-blue-200 transition-all duration-300 ease-out outline-none focus:outline-none focus:ring-2 focus:ring-[#4AD1F9] focus:border-transparent"
+              />
+            </div>
+            <div className="w-full">
+              <Input
+                placeholder="First Name"
+                type="text"
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                className="rounded-lg p-6 border-blue-200 transition-all duration-300 ease-out outline-none focus:outline-none focus:ring-2 focus:ring-[#4AD1F9] focus:border-transparent"
+              />
+            </div>{" "}
+            <div className="w-full">
+              <Input
+                placeholder="Last Name"
+                type="text"
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                className="rounded-lg p-6 border-blue-200 transition-all duration-300 ease-out outline-none focus:outline-none focus:ring-2 focus:ring-[#4AD1F9] focus:border-transparent"
+              />
+            </div>
+            <div className="w-full flex items-center gap-5 justify-center">
+              {colors.map((color, i) => (
+                <div
+                  className={`${color} h-8 w-8 rounded-full cursor-pointer transition-all duration-300 ${
+                    selectedColor === i
+                      ? "outline outline-white/50 outline-1"
+                      : ""
+                  }`}
+                  key={i}
+                  onClick={() => setSelectedColor(i)}
+                ></div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full mx-auto pb-3 flex justify-center">
+          <Button
+            onClick={saveChanges}
+            className="h-10 w-3/4  btn transition-all ease-out duration-300"
+          >
+            Save Changes
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
